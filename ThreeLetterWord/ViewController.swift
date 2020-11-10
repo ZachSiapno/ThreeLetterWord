36c2effd9962aa94b7ca9a206c67e44d87a0ca10
ZachSiapno <EWSB52@gmail.com>
2020-11-10 09:41:59 -0500

    MVP completed

--- ThreeLetterWord/ViewController.swift
+++ ThreeLetterWord/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet var wordLabels: [UILabel]!
+    
+    
+    var counter = 0
+    var currentLetter = "A"
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func stackTapped(_ sender: UITapGestureRecognizer) {
+        // where I tapped, the current letter can go to the correct label
+        let location = sender.location(in: stackView)
+        
+        print(location)
+        
+        for label in wordLabels
+        {
+            if label.frame.contains(location)
+            {
+                label.text = currentLetter
+            }
+        }
+        
+        //update the current letter
+        
+        counter += 1
+        if counter > 25
+        {
+            counter = 0
+        }
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+        
+        
+    }
+    
+    
 }
 
